5d9c29452e6130c46ab41cd02bb23cf427108c64
Alfredo <atakori@outlook.com>
2018-04-18 00:02:28 -0400

    added header.js component and styling

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import Header from './src/components/header';
 //create component
 const App = () => {
 	return (
-		<Header/>
+		<Header headerText='Albums'/>
 	)
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Platform } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
+	const { textStyle, viewStyle } = styles;
+
 	return (
-			<Text>Albums!</Text>
+			<View style={viewStyle}>
+				<Text style={textStyle}>{props.headerText}</Text>
+			</View>
 		)
+}
+
+//create css-like styling rules
+const styles = {
+	viewStyle: {
+		backgroundColor: '#F8F8F8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: 'black',
+		shadowOffset: { width: 0, height: 2},
+		position: 'relative',
+		...Platform.select({
+			ios:{
+			shadowOpacity: 0.5
+		},
+		android: {
+			elevation: 10
+		},
+		})
+	},
+	textStyle: {
+		fontSize: 20
+	}
 }
 
 //make components available to other parts of app
